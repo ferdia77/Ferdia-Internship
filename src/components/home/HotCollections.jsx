32b6c87b31f59442515d73cf563befe4ca7dcd4b
David <kingqueensuited22@gmai.com>
2025-03-27 18:25:32 +0100

    issue with carousel css

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -12,6 +12,7 @@ import SimpleSlider from "../../pages/Slider";
 
 const HotCollections = () => {
   const [hotCollections, setHotCollections] = useState([])
+  const [loading, setLoading] = useState(false)
 
   async function fetchHotCollections() {
     const response = await axios.get(
@@ -36,31 +37,68 @@ const HotCollections = () => {
             </div>
           </div>
           <SimpleSlider>
-          {hotCollections.map((hotCollection, index) => (
-            
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to="/item-details">
-                      <img src={hotCollection.nftImage} className="lazy img-fluid" alt="" />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to="/author">
-                      <img className="lazy pp-coll" src={hotCollection.authorImage} alt="" />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{hotCollection.title}</h4>
-                    </Link>
-                    <span>{hotCollection.code}</span>
+            {/* {
+              loading ? (
+                <div className="skeleton">
+                  <div className="skeleton__loading">
+                    <div className="skeleton__loading--state">
+                      Loading
+                    </div>
                   </div>
                 </div>
-              </div>
+              ) : (
+                {hotCollections.map((hotCollection, index) => (
             
-          ))}
+                  <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <img src={hotCollection.nftImage} className="lazy img-fluid" alt="" />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img className="lazy pp-coll" src={hotCollection.authorImage} alt="" />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{hotCollection.title}</h4>
+                        </Link>
+                        <span>{hotCollection.code}</span>
+                      </div>
+                    </div>
+                  </div>
+                
+              ))}
+              )}
+             */}
+            {hotCollections.map((hotCollection, index) => (
+            
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft_coll">
+                <div className="nft_wrap">
+                  <Link to="/item-details">
+                    <img src={hotCollection.nftImage} className="lazy img-fluid" alt="" />
+                  </Link>
+                </div>
+                <div className="nft_coll_pp">
+                  <Link to="/author">
+                    <img className="lazy pp-coll" src={hotCollection.authorImage} alt="" />
+                  </Link>
+                  <i className="fa fa-check"></i>
+                </div>
+                <div className="nft_coll_info">
+                  <Link to="/explore">
+                    <h4>{hotCollection.title}</h4>
+                  </Link>
+                  <span>{hotCollection.code}</span>
+                </div>
+              </div>
+            </div>
+          
+        ))}
           </SimpleSlider>
         </div>
       </div>
