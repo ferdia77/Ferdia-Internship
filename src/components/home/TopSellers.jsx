18181be033c95d89003765ca4b5dc8b47aab107c
David <kingqueensuited22@gmai.com>
2025-04-08 11:12:51 +0200

    made topsellers dynamic with API, added skeleton loading state with terniary operator,ready to merge

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+async function fetchTopSellers() {
+
+  const response = await axios.get(
+    "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+  );
+  //setLoading(false)
+  console.log(response)
+  setTopSellers(response.data);
+}
+
+useEffect(() => {
+  fetchTopSellers();
+}, [fetchTopSellers])
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -13,28 +34,49 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
+
+          {loading ? (
+            <div className="col-md-12">
             <ol className="author_list">
               {new Array(12).fill(0).map((_, index) => (
+                <li key={index}>
+                  <div className="author_list_pp">
+                      <Skeleton width={50} height={50} borderRadius={50} />
+                      <i className="fa fa-check"></i>
+                  </div>
+                  <div className="author_list_info">
+                    <Skeleton  width={120} height={20} borderRadius={2}/>
+                    <span><Skeleton width={50} height={20} borderRadius={2} /></span>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </div>
+          ) : (
+            <div className="col-md-12">
+            <ol className="author_list">
+              {topSellers.map((topSeller, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={topSeller.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{topSeller.authorName}</Link>
+                    <span>{topSeller.price}</span>
                   </div>
                 </li>
               ))}
             </ol>
           </div>
+          )}
+          
         </div>
       </div>
     </section>
