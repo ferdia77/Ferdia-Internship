4ad8cc58a25c9ae7f13d90c37ef6cbfa21f5dd4f
David <kingqueensuited22@gmai.com>
2025-04-08 12:53:59 +0200

    error line 21

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import CountDown from "../UI/CountDown";
 
 const ExploreItems = () => {
+  const [exploreItems, setExploreItems] = useState([]);
+  const[loading, setLoading] = useState(true);
 
+  async function fetchExploreItems() {
+    const response = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+    );
+    setLoading(false);
+    setExploreItems(response.data);
+  }
+
+  useEffect(() => {
+    fetchExploreItems
+  }, [fetchExploreItems]);
   
   return (
     <>
@@ -16,7 +31,9 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {loading ? (
+        <>
+          {new Array(8).fill(0).map((_, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -70,6 +87,67 @@ const ExploreItems = () => {
           </div>
         </div>
       ))}
+        </>
+      ) : (
+        <>
+          {exploreItems.map((exploreItem, index) => (
+        <div
+          key={index}
+          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+          style={{ display: "block", backgroundSize: "cover" }}
+        >
+          <div className="nft__item">
+            <div className="author_list_pp">
+              <Link
+                to="/author"
+                data-bs-toggle="tooltip"
+                data-bs-placement="top"
+              >
+                <img className="lazy" src={exploreItem.authorImage} alt="" />
+                <i className="fa fa-check"></i>
+              </Link>
+            </div>
+            <CountDown>{exploreItem.expireDate}</CountDown>
+
+            <div className="nft__item_wrap">
+              <div className="nft__item_extra">
+                <div className="nft__item_buttons">
+                  <button>Buy Now</button>
+                  <div className="nft__item_share">
+                    <h4>Share</h4>
+                    <a href="" target="_blank" rel="noreferrer">
+                      <i className="fa fa-facebook fa-lg"></i>
+                    </a>
+                    <a href="" target="_blank" rel="noreferrer">
+                      <i className="fa fa-twitter fa-lg"></i>
+                    </a>
+                    <a href="">
+                      <i className="fa fa-envelope fa-lg"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <Link to="/item-details">
+                <img src={exploreItem.nftImage} className="lazy nft__item_preview" alt="" />
+              </Link>
+            </div>
+            <div className="nft__item_info">
+              <Link to="/item-details">
+                <h4>{exploreItem.title}</h4>
+              </Link>
+              <div className="nft__item_price">{exploreItem.price}</div>
+              <div className="nft__item_like">
+                <i className="fa fa-heart"></i>
+                <span>{exploreItem.likes}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+        </>
+      )
+
+      }
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
           Load more
