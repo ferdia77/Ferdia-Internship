153f66a94771a851f42d36322b777e8600c1eef5
David <kingqueensuited22@gmai.com>
2025-04-15 19:22:59 +0200

    put AuthorItems loading State into Author with a second terniary operator

--- src/components/author/AuthorItemsSkeleton.jsx
+++ src/components/author/AuthorItemsSkeleton.jsx
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React from "react";
 import Skeleton from "../UI/Skeleton";
+import { Link } from "react-router-dom";
 
-const AuthorItems = ({ author, collection }) => {
+const AuthorItemsSkeleton = () => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {collection.map((authorNFT, index) => (
+          {new Array(8).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={author.authorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
+                  <Skeleton width={50} height={50} borderRadius={50} />
                 </div>
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -36,22 +31,21 @@ const AuthorItems = ({ author, collection }) => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={authorNFT.nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
+                  <Skeleton width="100%" height="100%" borderRadius={8} />
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>{authorNFT.title}</h4>
+                    <h4>
+                      <Skeleton width={30} height={16} borderRadius={4} />
+                    </h4>
                   </Link>
-                  <div className="nft__item_price">{authorNFT.price}</div>
+                  <div className="nft__item_price">
+                    <Skeleton width={24} height={16} borderRadius={4} />
+                  </div>
                   <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{author.likes}</span>
+                    <span>
+                      <Skeleton width={30} height={16} borderRadius={4} />
+                    </span>
                   </div>
                 </div>
               </div>
@@ -63,4 +57,4 @@ const AuthorItems = ({ author, collection }) => {
   );
 };
 
-export default AuthorItems;
+export default AuthorItemsSkeleton;
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -7,13 +7,14 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import Skeleton from "../components/UI/Skeleton";
+import AuthorItemsSkeleton from "../components/author/AuthorItemsSkeleton";
 
 const Author = () => {
   const [loading, setLoading] = useState(true);
   const [author, setAuthor] = useState({});
   const [collection, setCollection] = useState([]);
-  const { authorId } = useParams()
-  const [isFollowing, setIsFollowing] = useState(false)
+  const { authorId } = useParams();
+  const [isFollowing, setIsFollowing] = useState(false);
 
   async function fetchAuthors() {
     const response = await axios.get(
@@ -27,12 +28,12 @@ const Author = () => {
 
   useEffect(() => {
     fetchAuthors();
-    console.log(authorId)
+    console.log(authorId);
   }, []);
 
-   function followBtn() {
-    setIsFollowing((prev) => !prev)
-   }
+  function followBtn() {
+    setIsFollowing((prev) => !prev);
+  }
 
   return (
     <div id="wrapper">
@@ -54,11 +55,10 @@ const Author = () => {
                 <>
                   <div className="col-md-12">
                     <div className="d_profile de-flex">
-                      <Skeleton  width={70} height={70} borderRadius={50} />
+                      <Skeleton width={70} height={70} borderRadius={50} />
                       <div className="profile_follow de-flex">
                         <div className="de-flex-col">
-                          <Skeleton width={160} height={44} borderRadius={12}/>
-                          
+                          <Skeleton width={160} height={44} borderRadius={12} />
                         </div>
                       </div>
                     </div>
@@ -66,7 +66,61 @@ const Author = () => {
 
                   <div className="col-md-12">
                     <div className="de_tab tab_simple">
-                      <AuthorItems author={author} collection={collection} />
+                      {loading ? (
+                        
+                      <div className="de_tab_content">
+                        <div className="tab-1">
+                          <div className="row">
+                            {new Array(8).fill(0).map((_, index) => (
+                              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                                <div className="nft__item">
+                                  <div className="author_list_pp">
+                                    <Skeleton width={50} height={50} borderRadius={50} />
+                                  </div>
+                                  <div className="nft__item_wrap">
+                                    <div className="nft__item_extra">
+                                      <div className="nft__item_buttons">
+                                        <button>Buy Now</button>
+                                        <div className="nft__item_share">
+                                          <h4>Share</h4>
+                                          <a href="" target="_blank" rel="noreferrer">
+                                            <i className="fa fa-facebook fa-lg"></i>
+                                          </a>
+                                          <a href="" target="_blank" rel="noreferrer">
+                                            <i className="fa fa-twitter fa-lg"></i>
+                                          </a>
+                                          <a href="">
+                                            <i className="fa fa-envelope fa-lg"></i>
+                                          </a>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <Skeleton width="100%" height="100%" borderRadius={8} />
+                                  </div>
+                                  <div className="nft__item_info">
+                                    <Link to="/item-details">
+                                      <h4>
+                                        <Skeleton width={30} height={16} borderRadius={4} />
+                                      </h4>
+                                    </Link>
+                                    <div className="nft__item_price">
+                                      <Skeleton width={24} height={16} borderRadius={4} />
+                                    </div>
+                                    <div className="nft__item_like">
+                                      <span>
+                                        <Skeleton width={30} height={16} borderRadius={4} />
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                      ) : (
+                        <AuthorItems author={author} collection={collection} />
+                      )}
                     </div>
                   </div>
                 </>
@@ -98,7 +152,10 @@ const Author = () => {
                       <div className="profile_follow de-flex">
                         <div className="de-flex-col">
                           <div className="profile_follower">
-                             {isFollowing ? author.followers + 1 : author.followers} followers
+                            {isFollowing
+                              ? author.followers + 1
+                              : author.followers}{" "}
+                            followers
                           </div>
                           <button
                             onClick={followBtn}
